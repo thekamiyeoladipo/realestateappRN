93fcdfa0083c7769b52e94b759d0a9bcfc448a91
kamiye <kamiye31@gmail.com>
2025-08-05 23:20:52 +0100

    feat: TTAILWIND NOW WORKS = CONFIGURED THE tailwind.config.js file to target appropriate files

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router'
 import { Text, View } from "react-native";
 
+
 export default function Index() {
   return (
     <View
@@ -10,6 +11,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className='font-bold underline text-red-600'>Welcome to Tailwind in RN!👋</Text>
+      <Text className='text-5xl font-light tracking-wide'>Shey dada ni?</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import "./globals.css"
+import "./global.css"
 
 export default function RootLayout() {
   return <Stack />;
