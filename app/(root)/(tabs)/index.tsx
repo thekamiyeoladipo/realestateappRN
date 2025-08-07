0bca77969921881c03dd30822ca6a564e0570f23
kamiye <kamiye31@gmail.com>
2025-08-07 18:07:20 +0100

    feat: Sign In Screen ui developed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,15 +4,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className='font-bold text-5xl font-rubik  underline text-red-600'>Welcome to Tailwind in RN!👋</Text>
-      <Text className='text-5xl font-bold font-rubik-light  tracking-wide'>Shey dada ni?</Text>
+    <View>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {
+    // Handle Google login logic here
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
 
-export default SignIn
+        <View className="px-10">
+          <Text
+            className="text-base text-center uppercase
+          font-rubik text-black-200"
+          >
+            Welcome to Global Timi Properties
+          </Text>
+          <Text
+            className="text-3xl font-rubik-bold
+          text-black-300 text-center mt-2 capitalize"
+          >
+            Let's get you closer to{" \n"}
+            <Text className="text-primary-300 capitalize">your ideal home</Text>
+          </Text>
+
+          <Text className="text-center text-lg font-rubik text-black-200 mt-12">
+            Login in with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-300
+          rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex-row items-center justify-center ">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text
+                className="text-lg font-rubik-medium
+            text-black-300 ml-4"
+              >
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
