430d1adbda4a12c35142a165f80fb186b8a132d6
kamiye <kamiye31@gmail.com>
2025-08-08 16:55:46 +0100

    feat: add TabsLayout component with tab navigation and update Index component structure

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,12 @@
 import { Link } from 'expo-router'
 import { Text, View } from "react-native";
 
-
-export default function Index() {
+const Index = () => {
   return (
     <View className='flex-1 items-center justify-center gap-y-4'>
       <Text className='text-4xl underline font-rubik-extrabold'>Welcome to GTP!👋</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/[id]">Property</Link>
     </View>
-  );
+  )
 }
+
+export default Index
