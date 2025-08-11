babd8e3684385bf1921d32d63a5863e5a285370f
kamiye <kamiye31@gmail.com>
2025-08-11 22:32:39 +0100

    feat: add splash screen configuration and update StatusBar in Explore and Index components

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 
 const Explore = () => {
   return (
     <View className='flex-1 items-center justify-center'>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <Text>Explore</Text>
     </View>
   )
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'expo-router'
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 
 const Index = () => {
   return (
+    
     <View className='flex-1 items-center justify-center gap-y-4'>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <Text className='text-4xl underline font-rubik-extrabold'>Welcome to GTP!👋</Text>
     </View>
   )
