0e83da979e67afead17fc44404de7fed5844b08f
kamiye <kamiye31@gmail.com>
2025-08-18 00:16:18 +0100

    fix: refactor Search component for improved search handling and styling adjustments

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,34 +1,45 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import { Image } from 'react-native';
+import { Image, TextInput } from 'react-native';
 import icons from '@/constants/icons';
-import { TextInput } from 'react-native-gesture-handler';
-import { useGlobalSearchParams, usePathname } from 'expo-router'
+import { useDebouncedCallback } from 'use-debounce';
+
+import { useGlobalSearchParams, usePathname, router } from 'expo-router'
 
 const Search = () => {
    const path = usePathname();
    const params = useGlobalSearchParams<{ query?: string }>();
    const [search, setSearch] = useState(params.query);
 
+   const debouncedSearch =  useDebouncedCallback((text: string) => router.setParams({ query: text }), 500);
+
  const handleSearch = (text: string) => {
     setSearch(text);
     // Implement search logic here
+    debouncedSearch(text);
   }
 
   return (
-    <View className='flex flex-row items-center justify-between w-full px-4
-    bg-accent-100 border border-primary-100 mt-5 py-4 rounded-full'>
-        <View className='flex-1 flex flex-row items-center justify-start z-50'>
-            <Image className='size-5 mr-4' source={icons.search} />
-            <TextInput
-            value={search}
-            onChangeText={handleSearch} 
-            placeholder='Search Available Properties'
-            className='text-sm font-rubik text-black-300'/>
-        </View>
-      
+    <View
+      className="flex flex-row items-center justify-between w-full px-4
+    bg-accent-100 border border-primary-100 mt-5 py-2 rounded-full"
+    >
+      <View className="flex-1 flex flex-row items-center justify-start z-50">
+        <Image className="size-5 mr-4" source={icons.search} />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search Available Properties"
+          className="text-sm font-rubik text-black-300"
+        />
+      </View>
+      <TouchableOpacity>
+        <Image source={icons.filter}
+        className='size-5'
+        />
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 export default Search
