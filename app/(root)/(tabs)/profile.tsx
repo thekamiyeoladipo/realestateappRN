9f1fbcd0494f1f3541b67e856dfeb004c67ec163
kamiye <kamiye31@gmail.com>
2025-08-17 19:53:35 +0100

    fix: update avatar source to use user data and remove unused SplashScreen component

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -72,7 +72,7 @@ const Profile = () => {
         <View className="flex flex-row justify-center mt-5">
           <View className="flex flex-col items-center relative mt-5">
             <Image
-              source={images.avatar}
+              source={{ uri: user?.avatar }}
               className="size-44 relative rounded-full"
             />
             <TouchableOpacity className="absolute bottom-11 right-2">
