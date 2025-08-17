0b196bf7459ae13abd5648b9c920a5e8f49d86e5
kamiye <kamiye31@gmail.com>
2025-08-17 23:29:42 +0100

    feat: implement search component with input field and styling

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,8 @@
 import { View, Text } from 'react-native'
 import React, { useState } from 'react'
+import { Image } from 'react-native';
+import icons from '@/constants/icons';
+import { TextInput } from 'react-native-gesture-handler';
 import { useGlobalSearchParams, usePathname } from 'expo-router'
 
 const Search = () => {
@@ -13,8 +16,17 @@ const Search = () => {
   }
 
   return (
-    <View>
-      <Text className=''>Search init</Text>
+    <View className='flex flex-row items-center justify-between w-full px-4
+    bg-accent-100 border border-primary-100 mt-5 py-4 rounded-full'>
+        <View className='flex-1 flex flex-row items-center justify-start z-50'>
+            <Image className='size-5 mr-4' source={icons.search} />
+            <TextInput
+            value={search}
+            onChangeText={handleSearch} 
+            placeholder='Search Available Properties'
+            className='text-sm font-rubik text-black-300'/>
+        </View>
+      
     </View>
   )
 }
